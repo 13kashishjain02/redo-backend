d7a3ee81a9345dcccc95e23f37d08d1e38d1d912
13kashishjain02 <70149521+13kashishjain02@users.noreply.github.com>
2021-07-03 23:48:29 +0530

    registeration api corrected

--- account/api/views.py
+++ account/api/views.py
@@ -1,19 +1,30 @@
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from django.views.decorators.csrf import csrf_exempt
 
 from account.api.serializers import RegistrationSerializer
 from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view, permission_classes,authentication_classes
 
 # Register
 
 # Url: https://<your-domain>/api/register
-@api_view(['POST', ])
+
+@api_view(['POST',])
+@csrf_exempt
+@authentication_classes([])
+@permission_classes([])
 def userregister(request):
+    print("hello")
+    print("hello",request.method)
     if request.method == 'POST':
         serializer = RegistrationSerializer(data=request.data)
         data = {}
+        print("hiiii")
+        print(request.data)
+        print(serializer)
         if serializer.is_valid():
+            print("valid")
             account = serializer.save()
             data['response'] = 'successfully registered new user.'
             data['email'] = account.email
